416ba9d776732cfd4f2c1bced726bf5c453e51a7
vhamm <v.hamm@tarent.de>
2014-02-07 15:45:01 +0100

    [wordpress] fix auto activation

--- files/auto-activation.sql
+++ files/auto-activation.sql
@@ -1,1 +1,1 @@
-UPDATE wp_sitemeta SET meta_value = 'a:2:{s:21:"publish/ToPublish.php";i:1391781723;s:21:"osiamCon/osiamCon.php";i:1391781728;}' WHERE meta_id=21;
+UPDATE wp_sitemeta SET meta_value = 'a:2:{s:21:"publish/ToPublish.php";i:1391783926;s:21:"osiamCon/osiamCon.php";i:1391783931;}' WHERE meta_id=44;
